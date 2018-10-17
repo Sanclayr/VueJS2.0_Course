f83eab16f9db3277458ec8c8697fd6a7fcc92956
Sanclayr Vieira Sindarsic <sanclayr@gmail.com>
2018-10-17 20:20:30 -0300

    VueJS 2.0 Course
    Lesson 29 finalizing character behavior

---
+++ applications/starbase/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <h3>{{title}}</h3>
+    <character></character>
+  </div>
+</template>
+
+<script>
+import Character from "./components/Character.vue";
+
+export default {
+  name: "app",
+  data() {
+    return {
+      title: "Generate Your Team"
+    };
+  },
+  components: {
+    Character
+  }
+};
+</script>
