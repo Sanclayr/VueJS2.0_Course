5c068105cf55442b35ad71d43c870ae3e8bb4719
Sanclayr Vieira Sindarsic <sanclayr@gmail.com>
2018-10-19 14:05:03 -0300

    VueJS2.0Course
    Jokester App with vue and vuex

---
+++ applications/jokester/src/components/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <h3>Got jokes?</h3>
+    <button class="btn btn-primary" @click="addJoke">Add a Joke</button>
+    <button class="btn btn-primary" @click="initJokes">Add ten random jokes</button>
+    <br>
+    <span v-for="(type, index) in types" :key="index">
+      <input 
+      type="checkbox" 
+      :value="type" 
+      v-model="checkedTypes"
+      checked>
+      <label>{{type}}</label>&nbsp;
+    </span>
+    <br>
+    <div class="col-md-12">
+      <Joke
+      v-for="(joke, index) in $store.state.jokes"
+      :joke="joke"
+      :key="index"
+      :index="index"
+      v-show="checkedTypes.includes(joke.type)"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapActions } from "vuex";
+import Joke from "./Joke.vue";
+
+export default {
+  data() {
+    return {
+      types: ["general", "knock-knock", "programming"],
+      checkedTypes: ["general", "knock-knock", "programming"]
+    };
+  },
+  methods: mapActions(["initJokes", "addJoke"]),
+  components: {
+    Joke
+  }
+};
+</script>
