d34f07045f463a7b9689bae4fbf1ba87dad7dfef
Sanclayr Vieira Sindarsic <sanclayr@gmail.com>
2018-10-22 15:47:43 -0200

    VueJS2.0Course
    Curso Finalizado

--- applications/turnout/src/firebaseApp.js
+++ applications/turnout/src/firebaseApp.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase'
 
 const config = {
-    apiKey: "AIzaSyBeooAdSZO8rrTOSWDOQ5Eud1chXviMh7U",
+    apiKey: "",
     authDomain: "turnout-c3b2e.firebaseapp.com",
     databaseURL: "https://turnout-c3b2e.firebaseio.com",
     projectId: "turnout-c3b2e",
     storageBucket: "turnout-c3b2e.appspot.com",
-    messagingSenderId: "58630980999"
+    messagingSenderId: ""
 };
 
 export const firebaseApp = firebase.initializeApp(config)
